6234aabf4898f66ada4ee71c41971fc731d0aec6
Sandrolaxx <sandro--ramos@hotmail.com>
2022-07-15 20:34:48 -0300

    Create requests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AnswerModel from '../model/answer'
 import QuestionModel from '../model/question'
 import Question from './components/Question'
@@ -11,8 +11,28 @@ const questionMock = new QuestionModel(1, "Qual desses símbolos significa quilo
     AnswerModel.correct("KM")
 ]);
 
+const BASE_URL = "http://localhost:3000/api";
+
 export default function Home() {
-    const [question, setQuestion] = useState(questionMock);
+    const [questionIds, setQuestionIds] = useState<number[]>();
+    const [question, setQuestion] = useState<QuestionModel>(questionMock);
+
+    useEffect(() => {
+        loadSurvey()
+    }, []);
+
+    async function loadSurvey() {
+        const response = await fetch(`${BASE_URL}/survey`);
+        const questionIds = await response.json();
+
+        setQuestionIds(questionIds);
+    }
+
+    async function loadQuestion(questionId: number) {
+        const response = await fetch(`${BASE_URL}/question/${questionId}`);
+        const question = await response.json();
+
+    }
 
     function handleResponse(question: QuestionModel) {
         setQuestion(question);
@@ -25,8 +45,8 @@ export default function Home() {
     }
 
     return (
-        <div style={{display: "flex", height: "100vh", justifyContent: "center", alignItems: "center"}}>
-            <Survey question={question} isLast handleAnswered={() => handleResponse} handleNextStep={handleTimeOver}/>
-        </div>
+        <Survey question={question} isLast
+            handleAnswered={() => handleResponse}
+            handleNextStep={handleTimeOver} />
     )
 }
